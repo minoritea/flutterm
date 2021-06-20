02f322dc399e844f1e2b4170c255c6d408bff331
Minori <snowcrush@mail.0pt.jp>
2021-06-20 15:19:05 +0900

    add input field

---
+++ lib/terminal.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Terminal(),
+    );
+  }
+}
+
+class Terminal extends StatefulWidget {
+  const Terminal({Key? key}) : super(key: key);
+
+  @override
+  _TerminalState createState() => _TerminalState();
+}
+
+class _TerminalState extends State<Terminal> {
+  final _channel = WebSocketChannel.connect(
+    Uri.parse('ws://127.0.0.1:50505'),
+  );
+
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: SizedBox.expand(
+          child: Container(
+            // constraints: BoxConstraints.tightForFinite(),
+            color: Colors.blueGrey,
+            child: StreamBuilder<List<List<int>>>(
+              stream: _channel.stream.transform(
+                ScanStreamTransformer<dynamic, List<List<int>>>((acc, curr, i) {
+                  acc.add(curr as List<int>);
+                  return acc;
+                }, []),
+              ),
+              builder: (context, snapshot) {
+                final List<List<int>>? data = snapshot.data;
+                if (data == null) {
+                  return Text("");
+                }
+
+                final str = data.map((data) {
+                  final regexp = RegExp(r'\x1B');
+                  final str = utf8.decode(data).replaceAll(regexp, '^[');
+
+                  RegExp(r'[\x00-\x1F\x7F]').allMatches(str).forEach((m) {
+                    debugPrint("control chars");
+                    debugPrint(m[0]?.codeUnits.toString());
+                  });
+
+                  debugPrint('"' + str + '"');
+                  return str;
+                }).join("\n");
+                return Text(str);
+              },
+            ),
+          ),
+        ),
+      ),
+      TextField(
+        controller: _textController,
+        onSubmitted: (text) {
+          debugPrint(text);
+          _textController.clear();
+          _channel.sink.add(utf8.encode(text + "\n"));
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+      ),
+    ]);
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
+  }
+}
